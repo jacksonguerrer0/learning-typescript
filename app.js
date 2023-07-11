17a29f0d81c31ecdcab9d909857e8d096af558bf
jacksonguerrer0 <jacksonguerrerovega2@gmail.com>
2023-07-11 09:14:57 -0500

    config app

---
+++ app.js
@@ -0,0 +1,9 @@
+"use strict";
+(() => {
+    console.log('Hello');
+    const n = 10;
+    let arr = [];
+    // const demo = (msg) => {
+    //   console.log(msg)
+    // }
+})();
---
+++ app.ts
@@ -0,0 +1,10 @@
+(() => {
+  console.log('Hello')
+
+  const n: number = 10
+  let arr: any[] = []
+
+  // const demo = (msg) => {
+  //   console.log(msg)
+  // }
+})()
